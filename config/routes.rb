19165c2e3083cae54a6654114567109db2e5a64a
crisandryj <crisandryj@gmail.com>
2022-10-12 17:07:15 -0400

    created events resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/index'
-
+  resource :events
   get 'users/:id' => 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
