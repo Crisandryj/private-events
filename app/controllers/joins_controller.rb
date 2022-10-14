8492d8880e5c1cfa6a85758382bed97118735015
crisandryj <crisandryj@gmail.com>
2022-10-14 16:57:46 -0400

    turned off turbo for join view

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,23 +1,20 @@
 class JoinsController < ApplicationController
   def new
-    @user = User.new
+    @Join = Join.new
   end
 
   def create
-    @user = User.attended_events(event_params)
+    @attended_event = User.attended_events(event_params)
+    if @attended_event.save
+      redirect_to root_path, notice: "You've selected event to attend"
+    else
+      redirect_to root_path, alert: "did not save"
+    end
   end
-
-  def index
-    @joins = Join.all
-  end
-
-
-
-
 
   private
   def event_params
-    params.require(:event).permit(:title,:date)
+    params.require(:user).permit(:title,:date)
   end
 
 end
